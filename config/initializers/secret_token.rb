29266180a041e542145557fa2247ba07d16ce906
Josh Lindsey <josh@cloudspace.com>
2011-08-09 10:38:12 -0400

    Remove project files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Userchat::Application.config.secret_token = '9a0ff61cdafc4157c262218272056e0786b2541c2a2756bcc74e89091feb0fe7c2b50f96c35a0af9a0d516752624693cfe9bcd528bef0a724f771aef0ff99710'

1beceedeaac9c76fed696223eb5f7ce0a60a4fdd
Josh Lindsey <josh@cloudspace.com>
2011-08-23 10:33:10 -0400

    Update routes for Omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Userchat::Application.routes.draw do
-  get "welcome/index"
-
   root :to => 'welcome#index'
+
+  # Omniauth
+  match '/signin' => 'services#signin'
+  match '/signout' => 'services#signout'
+  match '/auth/:service/callback' => 'services#create'
+  match '/auth/failure' => 'services#failure'
+
+  resources :users, :only => [:index, :show]
+  resources :services, :only => [:index, :create, :destroy] do
+    collection do
+      get 'signin'
+      get 'signout'
+      get 'failure'
+    end
+  end
 end

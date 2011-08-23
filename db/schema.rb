cdd2395ddc5ee4ceb431c4a1487d5f3595a11456
Josh Lindsey <josh@cloudspace.com>
2011-08-23 10:48:20 -0400

    Generate Services model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110823144024) do
+ActiveRecord::Schema.define(:version => 20110823144215) do
+
+  create_table "services", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "uname"
+    t.string   "uemail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"

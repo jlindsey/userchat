213e474dc7ea6a753eac64dbb2a2cfcf58ca683e
Josh Lindsey <josh@cloudspace.com>
2011-08-23 10:02:44 -0400

    Add nav_item helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_item ctrl, action, title
+    opts = current_page?(:controller => ctrl, :action => action) ? { :class => 'active' } : {}
+
+    capture_haml do
+      haml_tag :li, opts do
+        haml_tag :a, title, { :href => url_for(:controller => ctrl, :action => action) }
+      end
+    end
+  end
 end
